355d248ba998f456bda9c74f6decf5eaebd4e7d7
Rami Gökhan Kıcı <gokhankici@gmail.com>
2017-01-31 15:13:31 -0800

    death watch

--- src/main/scala/Customer.scala
+++ src/main/scala/Customer.scala
@@ -11,9 +11,20 @@ class Customer(coffeeSource: ActorRef) extends Actor with ActorLogging {
   import Customer._
   import Barista._
   import EspressoCup._
+  import context.dispatcher
+
+  context.watch(coffeeSource)
+
   def receive = {
     case CaffeineWithdrawalWarning => coffeeSource ! EspressoRequest
     case (EspressoCup(Filled), Receipt(amount)) =>
       log.info(s"yay, caffeine for ${self}!")
+    case ComebackLater =>
+      log.info("grumble, grumble")
+      context.system.scheduler.scheduleOnce(300.millis) {
+        coffeeSource ! EspressoRequest
+      }
+    case Terminated(barista) =>
+      log.info("Oh well, let's find another coffeehouse...")
   }
 }
