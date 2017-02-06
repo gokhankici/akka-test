c38437e2af984b000d1088d40d92a2bcdf4bab4a
Rami Gökhan Kıcı <gokhankici@gmail.com>
2017-02-06 01:48:17 -0800

    mapreduce working

--- src/main/scala/mapreduce/MapReduceApp.scala
+++ src/main/scala/mapreduce/MapReduceApp.scala
@@ -5,14 +5,45 @@ import concurrent.duration._
 import akka.actor.SupervisorStrategy._
 import akka.actor.OneForOneStrategy
 
-class MapReduce extends Actor with ActorLogging {
-  val workerCount = 10
-  val jobCount = 15
+object MRStrategy {
+  val intensity: Int = 10
+  val interval: Duration = 1.second
+  val defaultDecider: Decider = {
+    case _: Exception => Restart
+  }
+}
 
-  override val supervisorStrategy =
-    OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1.second) {
-      case _: Exception => Restart
+class MRStrategy() extends SupervisorStrategy {
+  val decider = MRStrategy.defaultDecider
+
+  def handleChildTerminated(
+    context: ActorContext,
+    child: ActorRef,
+    children: Iterable[ActorRef]
+  ): Unit = ()
+
+  def processFailure(
+    context: ActorContext,
+    restart: Boolean,
+    child: ActorRef,
+    cause: Throwable,
+    stats: ChildRestartStats,
+    children: Iterable[ChildRestartStats]
+  ): Unit = {
+
+    context.stop(child)
+
+    if (restart) {
+      context.actorOf(Props[MapReduce])
     }
+  }
+}
+
+class MapReduce extends Actor with ActorLogging {
+  val workerCount = 3
+  val jobCount = 5
+
+  override val supervisorStrategy = new MRStrategy()
 
   override def preStart(): Unit = {
     context.actorOf(Props(classOf[Master], workerCount, jobCount))
